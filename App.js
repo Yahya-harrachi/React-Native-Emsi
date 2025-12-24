3d8dfdb8713a8fe222237954570599b290b4baec
Yahya Harrachi <harrachi.yahya0@gmail.com>
2025-12-24 17:34:47 +0100

    TP5 Commit

--- App.js
+++ App.js
@@ -1,72 +1,21 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-
-import HomeScreen from './Screens/HomeScreen';
-import DetailsScreen from './Screens/DetailsScreen';
-import SettingsScreen from './Screens/SettingsScreen';
-import AppBar from './Screens/AppBar';
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+import { NavigationContainer } from "@react-navigation/native";
+import AuthProvider, { AuthContext } from "./Context/AuthContext";
+import AppDrawer from "./Navigation/AppDrawer";
+import LoginScreen from "./Screens/LoginScreen";
+import { useContext } from "react";
 
 
-function HomeStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="Accueil"
-        component={HomeScreen}
-        options={{
-          headerStyle: { backgroundColor: '#007AFF' },
-          headerTintColor: '#fff',
-          headerTitleStyle: { fontWeight: 'bold' },
-        }}
-      />
-      <Stack.Screen
-        name="Details"
-        component={DetailsScreen}
-        options={{ title: 'Mes Détails Personnalisés' }}
-      />
-    </Stack.Navigator>
-  );
+function RootNavigator() {
+  const { user } = useContext(AuthContext);
+  return user ? <AppDrawer /> : <LoginScreen />;
 }
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <AppBar /> 
-
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarActiveTintColor: 'blue',
-          tabBarInactiveTintColor: 'gray',
-          tabBarStyle: { backgroundColor: '#f0f0f0' },
-          tabBarLabelStyle: { fontSize: 14 },
-        }}
-      >
-        <Tab.Screen
-          name="Maison"
-          component={HomeStack}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Paramètres"
-          component={SettingsScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Ionicons name="settings" size={size} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </AuthProvider>
   );
 }

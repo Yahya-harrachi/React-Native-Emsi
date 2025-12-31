5bfd632d1c4305376c8704352a8d6e116dee1e25
Yahya Harrachi <harrachi.yahya0@gmail.com>
2025-12-31 16:55:49 +0100

    commit TP 7

--- App.js
+++ App.js
@@ -1,25 +1,48 @@
-import { NavigationContainer } from "@react-navigation/native";
-import AuthProvider, { AuthContext } from "./Context/AuthContext";
-import AppDrawer from "./Navigation/AppDrawer";
-import LoginScreen from "./Screens/LoginScreen";
-import { useContext } from "react";
-
-import { Provider } from "react-redux";
-import { store } from "./Store/store";
-
-function RootNavigator() {
-  const { user } = useContext(AuthContext);
-  return user ? <AppDrawer /> : <LoginScreen />;
-}
-
-export default function App() {
+import { useEffect, useState, useContext } from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+import { initDB } from "./services/database";
+import { ThemeProvider, ThemeContext } from "./Context/ThemeContext";
+import TodoListOfflineScreen from "./Screens/TodoListOfflineScreen";
+function MainApp() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <Provider store={store}>
-      <AuthProvider>
-        <NavigationContainer>
-          <RootNavigator />
-        </NavigationContainer>
-      </AuthProvider>
-    </Provider>
+    <View
+      style={[
+        styles.container,
+        theme === "dark" ? styles.dark : styles.light,
+      ]}
+    >
+      <TodoListOfflineScreen />
+    </View>
   );
 }
+export default function App() {
+  const [dbReady, setDbReady] = useState(false);
+  useEffect(() => {
+    const prepareDb = async () => {
+      await initDB();
+      // attendre SQLite
+      setDbReady(true); // OK pour afficher l’app
+    };
+    prepareDb();
+  }, []);
+  if (!dbReady) {
+    return <ActivityIndicator size="large" />;
+  }
+  return (
+    <ThemeProvider>
+      <MainApp />
+    </ThemeProvider>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+  },
+  light: {
+    backgroundColor: "#ffffff",
+  }, dark: {
+    backgroundColor: "#121212",
+  },
+});
